3932b74f7545effbc91db9c0209a09bae8e37bcc
Skrahul06 <notjoker85@gmail.com>
2026-06-24 09:51:44 +0530

    Update API_BASE to production URL

--- js/newentry.js
+++ js/newentry.js
@@ -1,7 +1,8 @@
 // js/newentry.js
 import { loadSidebar } from './layout.js';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://technocraft-saas.onrender.com/api';
+//const API_BASE = 'http://localhost:5000/api';
 let availableProductsCache = []; 
 
 function getAuthHeaders() {
