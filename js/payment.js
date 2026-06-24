f5dea1ccb85e2225cba6194c8309a2d04d365d5a
Skrahul06 <notjoker85@gmail.com>
2026-06-24 09:52:16 +0530

    Update API_BASE URL to production endpoint

--- js/payment.js
+++ js/payment.js
@@ -1,7 +1,8 @@
 // js/payments.js
 import { loadSidebar } from './layout.js';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://technocraft-saas.onrender.com/api';
+//const API_BASE = 'http://localhost:5000/api';
 
 function getAuthHeaders() {
     const token = localStorage.getItem('token');
