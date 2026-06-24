9f0599e8563a237ec74458b29b3a58042f730a5e
Skrahul06 <notjoker85@gmail.com>
2026-06-24 09:57:04 +0530

    Update API base URL for production environment

--- js/api.js
+++ js/api.js
@@ -1,5 +1,7 @@
 // js/api.js
-const API_BASE_URL = 'http://localhost:5000/api';
+
+ const API_BASE = 'https://technocraft-saas.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:5000/api';
 
 export async function fetchChallans() {
     try {
